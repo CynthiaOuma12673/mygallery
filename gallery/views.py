cfa8e36768ccb002ccffcd0e7dd6d06e2becfbb5
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-27 16:28:27 +0300

    it will create the welcome function for the homepage

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from tkinter import Image
+from django.shortcuts import render,redirect
+from django.http import Http404
+from .models import Photo, Location, Category
 
 # Create your views here.
 def welcome(request):
-    return render(request,'welcome.html')
+    images = Photo.objects.all()
+    locations = Location.all_locations()
+    return render(request,'welcome.html', {'images':images, 'locations':locations})
+
+
