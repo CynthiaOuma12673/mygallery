749fe2e96c866a41fd92743580eef6c9ad3f8496
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-27 15:35:18 +0300

    it will create the class photo and add all its functions

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -38,4 +39,31 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Photo(models.Model):
+    image = models.ImageField(upload_to= 'images/')
+    description = models.TextField()
+    name = models.CharField(max_length=100)
+    upload_date = models.DateTimeField(auto_now_add = True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    
+    def save_image(self):
+        self.save()
 
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id = id).first()
+        return image
+
+    @classmethod
+    def search_image(cls, search_category):
+        images = cls.objects.filter(category_name_icontains = search_category)
+        return images
+    
+    @classmethod
+    def filter_by_location(cls, search_location):
+        location = cls.objects.filter(location_name = search_location).all()
+        return location
