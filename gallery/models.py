dd0ecdf9e27ac3371176b356aad1e0da095d247e
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-27 15:16:57 +0300

    it will add the class Location

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def all_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    @classmethod
+    def update_location(cls, id, name):
+        cls.objects.filter(id = id).update(ame = name)
+
+    def __str__(self):
+        return self.name
--- photo/settings.py
+++ photo/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for photo project.
 
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
