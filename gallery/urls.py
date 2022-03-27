8eb1953ffda002c233fa3ba2ef94b8b9df25fbd9
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-27 17:03:04 +0300

    it will add the debug statement to the urls page

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
         url('',views.welcome,name='galleryHome'),
         url('search/', views.get_category,name ='category'),
         url('location/<str:search_location>/',views.get_location,name='location'),
         url('image/<int:image_id>/',views.get_image,name='image'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
