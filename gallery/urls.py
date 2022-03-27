2bed520a4238ca455a4b944e8294f0aeb1a73684
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-03-27 17:01:07 +0300

    it will add url to route through my pages

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
         url('',views.welcome,name='galleryHome'),
+        url('search/', views.get_category,name ='category'),
+        url('location/<str:search_location>/',views.get_location,name='location'),
+        url('image/<int:image_id>/',views.get_image,name='image'),
 ]
